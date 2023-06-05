f6f746c2f625aa64dec7b4c9c36b5552e4b911a3
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-06-05 16:17:30 +0200

    Creacion del proyecto

---
+++ src/main/java/Programacion/EntidadFinanciera/App.java
@@ -0,0 +1,13 @@
+package Programacion.EntidadFinanciera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
