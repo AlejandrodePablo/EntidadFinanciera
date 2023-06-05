1772d932ce150ace0ceb88102ef4b68577320f4a
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-06-05 17:04:37 +0200

    creacion de las carpetas de datos con dos ejemplos

---
+++ src/main/java/Programacion/EntidadFinanciera/data/BankAccountDataRepository.java
@@ -0,0 +1,17 @@
+package Programacion.EntidadFinanciera.data;
+/*
+import Programacion.EntidadFinanciera.data.local.BankAccountFileDataSource;
+import Programacion.EntidadFinanciera.domain.models.BankAccount;
+import Programacion.EntidadFinanciera.domain.useCase.BankAccountRepository;
+
+public class BankAccountDataRepository implements BankAccountRepository {
+    private BankAccountFileDataSource bankAccountFileDataSource;
+    public BankAccountDataRepository(BankAccountFileDataSource bankAccountFileDataSource){
+        this.bankAccountFileDataSource=bankAccountFileDataSource;
+    }
+
+    @Override
+    public void save(BankAccount bankAccount) {
+
+    }
+}*/
