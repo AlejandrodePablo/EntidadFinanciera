176570771dc862ff3cc9b3f1ae5e3898d3213f07
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-06-05 16:52:08 +0200

    test1

--- src/main/java/Programacion/EntidadFinanciera/App.java
+++ src/main/java/Programacion/EntidadFinanciera/App.java
@@ -1,13 +1,9 @@
 package Programacion.EntidadFinanciera;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hola rey" );
     }
 }
